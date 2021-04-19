d09c30a0f67301cc9cc823361198434923825e32
Ubuntu <ubuntu@ip-172-31-39-30.eu-west-2.compute.internal>
2021-04-19 08:37:02 +0000

    Minor changes.

--- rep-database.sql
+++ rep-database.sql
@@ -190,36 +190,36 @@ CREATE TABLE Avisos_Greve (
 
 CREATE TRIGGER Mencoes_BTE_Org_Sindical_update AFTER UPDATE ON Mencoes_BTE_Org_Sindical BEGIN
 		UPDATE Mencoes_BTE_Org_Sindical
-		SET    URL = "http://bte.gep.msess.gov.pt/completos/" [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
+		SET    URL = "http://bte.gep.msess.gov.pt/completos/" || [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
 		WHERE ID_Organizacao_Sindical = [NEW].ID_Organizacao_Sindical; 
 END;
 
 CREATE TRIGGER Mencoes_BTE_Org_Sindical_insert AFTER INSERT ON Mencoes_BTE_Org_Sindical BEGIN
 		UPDATE Mencoes_BTE_Org_Sindical
-		SET    URL = "http://bte.gep.msess.gov.pt/completos/" [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
+		SET    URL = "http://bte.gep.msess.gov.pt/completos/" || [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
 		WHERE ID_Organizacao_Sindical = [NEW].ID_Organizacao_Sindical; 
 END;
 
 CREATE TRIGGER Mencoes_BTE_Org_Patronal_update AFTER UPDATE ON Mencoes_BTE_Org_Patronal BEGIN
 		UPDATE Mencoes_BTE_Org_Patronal
-		SET    URL = "http://bte.gep.msess.gov.pt/completos/" [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
+		SET    URL = "http://bte.gep.msess.gov.pt/completos/" || [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
 		WHERE ID_Organizacao_Patronal = [NEW].ID_Organizacao_Patronal; 
 END;
 
 CREATE TRIGGER Mencoes_BTE_Org_Patronal_insert AFTER INSERT ON Mencoes_BTE_Org_Patronal BEGIN
 		UPDATE Mencoes_BTE_Org_Patronal
-		SET    URL = "http://bte.gep.msess.gov.pt/completos/" [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
+		SET    URL = "http://bte.gep.msess.gov.pt/completos/" || [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
 		WHERE ID_Organizacao_Patronal = [NEW].ID_Organizacao_Patronal; 
 END;
 
 CREATE TRIGGER Actos_Negociacao_Colectiva_update AFTER UPDATE ON Actos_Negociacao_Colectiva BEGIN
 		UPDATE Actos_Negociacao_Colectiva
-		SET    URL = "http://bte.gep.msess.gov.pt/completos/" [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
+		SET    URL = "http://bte.gep.msess.gov.pt/completos/" || [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
 		WHERE ID = [NEW].ID; 
 END;
 
 CREATE TRIGGER Actos_Negociacao_Colectiva_insert AFTER INSERT ON Actos_Negociacao_Colectiva BEGIN
 		UPDATE Actos_Negociacao_Colectiva
-		SET    URL = "http://bte.gep.msess.gov.pt/completos/" [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
+		SET    URL = "http://bte.gep.msess.gov.pt/completos/" || [NEW].Ano || "/bte" || [NEW].Numero || "_" || [NEW].Ano || ".pdf"
 		WHERE ID = [NEW].ID; 
 END;
